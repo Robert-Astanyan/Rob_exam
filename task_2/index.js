84468c7a66288507a3326bd2c75ca7c70a4f58ca
Robert-Astanyan <robast43@gmail.com>
2021-12-11 14:37:31 +0400

    Task-2

---
+++ task_2/index.js
@@ -0,0 +1,18 @@
+const array = [
+    { "first_name": "Armen", "last_name": "Amiryan", "age": "33" },
+    { "first_name": "Karine", "last_name": "Karapetyan", "age": "23" },
+    { "first_name": "Manushak", "last_name": "Voskanyan", "age": "20" },
+    { "first_name": "Eghiazar", "last_name": "Avetisyan", "age": "18" },
+    { "first_name": "Hrant", "last_name": "Tokhatyan", "age": "22" },
+    { "first_name": "Zarzand", "last_name": "Mnatsakanyan", "age": "66" },
+    { "first_name": "Murad", "last_name": "Muradyan", "age": "58" },
+    { "first_name": "Ashkhen", "last_name": "Khanamiryan", "age": "48" }
+]
+
+let a = array.map(myFunction)
+
+function myFunction (lem){
+    return[lem.first_name,lem.last_name].join("--")
+}
+
+console.log(a);
