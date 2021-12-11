e6c3530f06cf8a8911c801dd135c9e4b76f3b7d6
Robert-Astanyan <robast43@gmail.com>
2021-12-11 14:59:01 +0400

    Task-4

---
+++ task_4/index.js
@@ -0,0 +1,7 @@
+let names = [ "Mnatsakan", "Karapetyan","Baghdasaryan","Abelyan","Isahakyan"];
+let surnames = ["Hovhannes", "Avetiq", "Daniel","Grigor"];
+
+const b = [...names,...surnames]
+
+console.log(b);
+
